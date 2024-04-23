f1adbe1849ba2bf835d08ccf22fe495f870d1033
Reigner Lastimosa <lastimosareignerc@gmail.com>
2024-04-23 15:17:49 +0800

    test for deployment

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from predict_page import show_predict_page
-from explore_page import show_explore_page
+from salaryprediction import show_predict_page
 
+st.set_page_config(page_title="Salary Prediction App")
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ("Predict Salary",))
 
-if page == "Predict":
+if page == "Predict Salary":
     show_predict_page()
-else:
-    show_explore_page()
--- predict_page.py
+++ predict_page.py
@@ -2,58 +2,46 @@ import streamlit as st
 import pickle
 import numpy as np
 
-
-def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
+def load_model(file_path='saved_steps.pkl'):
+    with open(file_path, 'rb') as file:
         data = pickle.load(file)
     return data
 
-data = load_model()
-
-regressor = data["model"]
-le_country = data["le_country"]
-le_education = data["le_education"]
+def predict_salary(country, education_level, experience_years, file_path='saved_steps.pkl'):
+    data = load_model(file_path)
+    regressor = data["model"]
+    le_country = data["le_country"]
+    le_education = data["le_education"]
+    
+    # Pre-process input data
+    X = np.array([[country, education_level, experience_years]])
+    X[:, 0] = le_country.transform(X[:,0])
+    X[:, 1] = le_education.transform(X[:,1])
+    X = X.astype(float)
+    
+    # Make prediction
+    salary = regressor.predict(X)
+    return salary[0]
 
 def show_predict_page():
     st.title("Software Developer Salary Prediction")
-
     st.write("""### We need some information to predict the salary""")
 
     countries = (
-        "United States",
-        "India",
-        "United Kingdom",
-        "Germany",
-        "Canada",
-        "Brazil",
-        "France",
-        "Spain",
-        "Australia",
-        "Netherlands",
-        "Poland",
-        "Italy",
-        "Russian Federation",
-        "Sweden",
+        "United States", "India", "United Kingdom", "Germany", "Canada",
+        "Brazil", "France", "Spain", "Australia", "Netherlands", "Poland",
+        "Italy", "Russian Federation", "Sweden"
     )
 
     education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
+        "Less than a Bachelors", "Bachelor’s degree", "Master’s degree", "Post grad"
     )
 
     country = st.selectbox("Country", countries)
-    education = st.selectbox("Education Level", education)
-
-    expericence = st.slider("Years of Experience", 0, 50, 3)
+    education_level = st.selectbox("Education Level", education)
+    experience_years = st.slider("Years of Experience", 0, 50, 3)
 
     ok = st.button("Calculate Salary")
     if ok:
-        X = np.array([[country, education, expericence ]])
-        X[:, 0] = le_country.transform(X[:,0])
-        X[:, 1] = le_education.transform(X[:,1])
-        X = X.astype(float)
-
-        salary = regressor.predict(X)
-        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+        salary = predict_salary(country, education_level, experience_years)
+        st.subheader(f"The estimated salary is ${salary:.2f}")
